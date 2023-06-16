ff055fc1197982773e84de1b0a031d3f728a4ac6
Ihar Yurkevich <igorurkevich@gmail.com>
2023-06-16 19:40:47 +0300

    new homework 2

---
+++ Домашняя работа 2/10/Program.cs
@@ -0,0 +1,16 @@
+﻿// 10
+Console.Write("Введите число а=");
+int a = int.Parse(Console.ReadLine());
+
+int b = 0;
+
+if (a > 99 && a < 1000)
+{
+   b = (a / 10) % 10;
+   
+   Console.WriteLine($"Вторая цифра числа {a}   {b}");
+}
+else
+{
+    Console.WriteLine("число не входит в диапазон трехзначных чисел");
+}
---
+++ Домашняя работа 2/13/Program.cs
@@ -0,0 +1,20 @@
+﻿//13
+Console.Write("Введите число а=");
+int a = int.Parse(Console.ReadLine());
+
+int b = 0;
+
+if (a <100)
+{
+   
+   Console.WriteLine("вы ввели не трехзначное число");
+}
+else
+{
+    while(a > 999)
+    {
+    a /= 10;
+    }
+    b = a % 10;
+     Console.WriteLine(b);
+}
