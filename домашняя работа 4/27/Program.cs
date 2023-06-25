127a6ed549e90087a5b474ba1073fc14c1ac4dd0
IgorUrkevich <igorurkevich@gmail.com>
2023-06-25 11:48:19 +0300

    add hw 4

---
+++ домашняя работа 4/27/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Write("Ведите число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+int resualt = 0;
+
+while (numberA > 0)
+{
+    resualt = resualt + (numberA % 10);
+    numberA = numberA / 10 ;
+}
+
+Console.WriteLine($"cумма цифр введенного числа равна {resualt} ");
